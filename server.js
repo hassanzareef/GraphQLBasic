ebf754593f09f9f5a77687c90fa62296dae16264
unknown <hassan.zareef@gmail.com>
2023-06-19 15:55:35 -0400

    test

--- server.js
+++ server.js
@@ -3,20 +3,70 @@ const expressGraphQL = require('express-graphql')
 const{
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLInt
 } = require('graphql')
 const app = express()
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields : () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World'
+const directors = [
+{id: 1, name: 'Christopher Nolan'},
+{id: 2, name: 'Zack Snyder'},
+{id: 3, name: 'Russo Brothers'}
+]
+
+const movies = [
+    {id: 1, name: 'ABC121', directorId: 1},
+    {id: 2, name: 'ABC122', directorId: 1},
+    {id: 3, name: 'ABC123', directorId: 1},
+    {id: 4, name: 'ABC124', directorId: 2},
+    {id: 5, name: 'ABC125', directorId: 2},
+    {id: 6, name: 'ABC126', directorId: 2},
+    {id: 7, name: 'ABC127', directorId: 3},
+    {id: 8, name: 'ABC128', directorId: 3},
+    {id: 9, name: 'ABC129', directorId: 3}
+]
+
+const MovieType = new GraphQLObjectType({
+    name: 'Movie',
+    description: 'This represents a movie made by a director',
+    fields: () => ({
+        id : {type : GraphQLNonNull(GraphQLInt)},
+        name : {type: GraphQLNonNull(GraphQLString)},
+        directorId : {type: GraphQLNonNull(GraphQLInt)},
+        director: {
+            type: DirectorType,
+            resolve : (movie) => {
+                return directors.find(director => director.id === movie.directorId)
             }
-        })
+        }
     })
+})
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    description: 'This represents a director of a movie',
+    fields: () => ({
+        id : {type : GraphQLNonNull(GraphQLInt)},
+        name : {type: GraphQLNonNull(GraphQLString)},
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name : 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        movies: {
+            type: new GraphQLList(MovieType),
+            description: 'List of Movies',
+            resolve: () => movies
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
 })
 
 app.use('/graphql', expressGraphQL.graphqlHTTP({
